c20722a3e81d727b91b251fe0c79d6c3f8a15f7f
jolerus <henrik.jolerus@gmail.com>
2023-07-31 15:15:04 +0100

    add hover feature state switching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,45 +10,34 @@ import { MAPBOX_ACCESS_TOKEN } from '../../mapboxtoken';
 export default function Home() {
   const mapRef = useRef<MapRef>(null);
 
+  let hoveredZoneId: number | null = null;
   const handleMouseMove = (event: MapLayerMouseEvent) => {
-    console.log("mousemove");
-    if (mapRef.current && event.features && event.features.length > 0 && event.features[0].properties) {
+    if (mapRef.current && event.features && event.features.length > 0 && event.features[0].properties && event.features[0]?.id) {
       mapRef.current.getCanvas().style.cursor = 'pointer';
-      console.log(event.features[0]);
-    
-      // If quakeID for the hovered feature is not null,
-      // use removeFeatureState to reset to the default behavior
-      // if (quakeID) {
-      //   map.removeFeatureState({
-      //     source: 'earthquakes',
-      //     id: quakeID
-      //   });
-      // }
-    
-      // quakeID = event.features[0].id;
-    
-      // // When the mouse moves over the earthquakes-viz layer, update the
-      // // feature state for the feature under the mouse
-      // map.setFeatureState(
-      //   {
-      //     source: 'earthquakes',
-      //     id: quakeID
-      //   },
-      //   {
-      //     hover: true
-      //   }
-      // );
+
+      if (hoveredZoneId) {
+        mapRef.current.removeFeatureState(
+          {
+            source: 'zones-src',
+            id: hoveredZoneId
+          }
+        );
+      }
+
+      hoveredZoneId = Number(event.features[0].id);
+
+      console.log("2")
+      mapRef.current.setFeatureState(
+        {
+          source: 'zones-src',
+          id: hoveredZoneId
+        },
+        {
+          hover: true
+        }
+      );
     }
   }
-
-
-  const onMapLoad = useCallback(() => {
-    if (mapRef.current) {
-      mapRef.current.on('mousemove', 'earthquakes-viz', (event) => {
-
-      });
-    }
-  }, []);
 
   return (
     <div className="flex w-full h-full">
@@ -57,17 +46,18 @@ export default function Home() {
       </Head>
       <Map
         ref={mapRef}
-        onMouseMove={handleMouseMove}
+        onMouseMove={(e) => handleMouseMove(e)}
         initialViewState={{
           latitude: 51.48,
           longitude: -0.115,
           zoom: 9.5
         }}
+        interactiveLayerIds={["zones"]}
         style={{left: 0, width: "100vw", height: "100vh"}}
         mapStyle="mapbox://styles/jolerus/clh4z2ham00pl01quabbchxqc"
         mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
       >
-        <Source id="zones" type="geojson" data={zones}>
+        <Source id="zones-src" type="geojson" data={zones}>
           <Layer
             id="zones"
             type="line"
