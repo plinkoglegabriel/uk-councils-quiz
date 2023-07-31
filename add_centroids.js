54ff4c3e23e5b138d84fa05d38caee650494b186
jolerus <henrik.jolerus@gmail.com>
2023-07-31 17:22:11 +0100

    add finished centroid script

--- add_centroids.js
+++ add_centroids.js
@@ -1,13 +1,26 @@
 const fs = require('fs');
 const turf = require('turf')
 
-fs.readFile('zones.json', 'utf8', (err, data) => {
+fs.readFile('london_zones.json', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
 
   const obj = JSON.parse(data);
-  const centroid = turf.centroid(obj.features[0]);
-  console.log(centroid);
+  const centroids = [];
+  for (var i = 0; i < obj.features.length; i++) {
+    let centroid = turf.centroid(obj.features[i]);
+    centroid.id = obj.features[i].id;
+    centroid.properties = obj.features[i].properties;
+    centroids.push(centroid);
+  }
+  console.log(centroids);
+  fs.writeFile('london_centroids.json', JSON.stringify(centroids), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Data written to file');
+  });
 });
